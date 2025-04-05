5fb957973339e337440c19a3b8484d9aa6e7305d
gustavovenerando <gvhenrique@hotmail.com>
2025-04-05 10:21:41 -0300

    adapter

---
+++ patterns/src/Adapter/useCase/index.ts
@@ -0,0 +1,40 @@
+import { ITarget } from "./interface";
+
+/*
+Its about making two interfaces that are imcompatible, compatible
+Adapter wraps something to adapt to a different interface
+Takes as input multiple kinds of interfaces and output one interface
+The intent is not changing original behavior
+*/
+
+class Client {
+    request(): void {
+        console.log('Making normal request')
+    }
+}
+
+// Class to be adapted
+class Adaptee {
+    specificRequest(): void {
+        console.log('Making specific request');
+    }
+}
+
+class Adapter implements ITarget {
+    constructor(
+        private adaptee: Adaptee
+    ){ }
+
+    request(): void {
+        this.adaptee.specificRequest();
+    }
+}
+
+// Normal usage
+const client = new Client();
+client.request();
+
+// Adapting similar behavior
+const adaptee = new Adaptee();
+const target = new Adapter(adaptee)
+target.request();
