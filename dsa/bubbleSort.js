86c4d907475e00564672dd9eb4d9db9b380dab1d
gustavovenerando <gvhenrique@hotmail.com>
2025-04-14 22:16:06 -0300

    bubble sort

---
+++ dsa/bubbleSort.js
@@ -0,0 +1,29 @@
+function bubbleSort(arr) {
+    let isSorted = false;
+    let lastIndexSorted = arr.length - 1;
+
+    while (!isSorted) {
+        isSorted = true;
+        for (let i = 0; i < lastIndexSorted; i++) {
+            if (arr[i] > arr[i + 1]) {
+                let currTemp = arr[i];
+                let nextTemp = arr[i + 1];
+
+                arr[i] = nextTemp;
+                arr[i + 1] = currTemp;
+
+                isSorted = false;
+            }
+        }
+        
+        lastIndexSorted -= 1;
+    }
+
+    return arr;
+}
+
+const arr = [5, 134, 44, 51, 1, 31, 200, 12, 22, 3];
+
+bubbleSort(arr);
+
+console.log(arr);
